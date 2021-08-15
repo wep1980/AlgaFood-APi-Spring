7d39c5d2f24307afc60328b9a76ad4a392290cef
wep1980 <wepbike@gmail.com>
2021-08-15 08:53:32 -0300

    Adicionando recurso com POST

--- src/main/java/br/com/wepdev/api/controller/CozinhaController.java
+++ src/main/java/br/com/wepdev/api/controller/CozinhaController.java
@@ -3,11 +3,15 @@ package br.com.wepdev.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.wepdev.api.model.CozinhasXmlWrapper;
@@ -40,12 +44,7 @@ public class CozinhaController {
 	   return new CozinhasXmlWrapper(repository.listar());
 	}
 	
-	/**
-	 * ResponseEntity possui um metodo status, que define o status da resposta que sera devolvido e devolvendo um corpo
-	 * que nesse caso e a cozinha
-	 * @param cozinhaId
-	 * @return
-	 */
+
 	@GetMapping("/{cozinhaId}")
 	public ResponseEntity<Cozinha> buscarPorId(@PathVariable Long cozinhaId) {
 		Cozinha cozinha = repository.buscarPorId(cozinhaId);
@@ -55,8 +54,41 @@ public class CozinhaController {
 		}
 		//return ResponseEntity.status(HttpStatus.NOT_FOUND).build(); // Se nao existir o Id retorna um NOT FOUND e sem corpo.
 		return ResponseEntity.notFound().build(); // Atalho para a linha acima
-	
-		
 	}
+	
+	
+	/**
+	 * @RequestBody Cozinha -> o RequestBody vai receber o corpo da Cozinha
+	 * @param cozinha
+	 */
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED) // Status 201, recurso criado
+	public Cozinha adicionar(@RequestBody Cozinha cozinha) {
+		return repository.salvarOuAtualizar(cozinha); // Retona a cozinha no corpo
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
