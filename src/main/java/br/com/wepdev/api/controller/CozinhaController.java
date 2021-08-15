7f9afb56f1ed97ef22f31a0135442f20bb60b8eb
wep1980 <wepbike@gmail.com>
2021-08-15 07:50:33 -0300

    @ResponseStatus status HTTP

--- src/main/java/br/com/wepdev/api/controller/CozinhaController.java
+++ src/main/java/br/com/wepdev/api/controller/CozinhaController.java
@@ -3,10 +3,12 @@ package br.com.wepdev.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.wepdev.api.model.CozinhasXmlWrapper;
@@ -39,7 +41,7 @@ public class CozinhaController {
 	   return new CozinhasXmlWrapper(repository.listar());
 	}
 	
-	
+	@ResponseStatus(HttpStatus.CREATED) // Alterando a resposta do status HTTP
 	@GetMapping("/{cozinhaId}")
 	public Cozinha buscarPorId(@PathVariable Long cozinhaId) {
 		return repository.buscarPorId(cozinhaId);
