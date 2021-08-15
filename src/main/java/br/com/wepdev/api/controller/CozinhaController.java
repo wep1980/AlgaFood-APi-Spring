e0028bb8748d7d08c7d76fff4829785a3c0b90d2
wep1980 <wepbike@gmail.com>
2021-08-14 22:12:47 -0300

    Content negotiation para retornar JSON ou XML

--- src/main/java/br/com/wepdev/api/controller/CozinhaController.java
+++ src/main/java/br/com/wepdev/api/controller/CozinhaController.java
@@ -3,6 +3,7 @@ package br.com.wepdev.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +15,7 @@ import br.com.wepdev.domain.repository.CozinhaRepository;
 //@ResponseBody // As Respostas dos metedos desse controlador devem ir na resposta da requisicao
 //@Controller // Controlador REST
 @RestController // Substitue as 2 anotacoes acima
-@RequestMapping(value = "/cozinhas")
+@RequestMapping(value = "/cozinhas") //, produces = MediaType.APPLICATION_JSON_VALUE) // Toda a classe produz JSON
 public class CozinhaController {
 	
 	
@@ -22,9 +23,14 @@ public class CozinhaController {
 	private CozinhaRepository repository;
 	
 	
-	@GetMapping
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE) // Produz Json - Anotacao colocada no escopo da classe
 	public List<Cozinha> listar(){
 		return repository.listar();
 	}
+	
+//	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE) // Produz Json - Anotacao colocada no escopo da classe
+//	public List<Cozinha> listar(){
+//		return repository.listar();
+//	}
 
 }
