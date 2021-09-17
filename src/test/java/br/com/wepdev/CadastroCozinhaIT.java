114a669c0028d3e0b26bb3c173d564b6cce16c85
wep1980 <wepbike@gmail.com>
2021-09-17 20:46:33 -0300

    Adicionando plugin no pom.xml para controle maior de build e teste de integração

--- src/test/java/br/com/wepdev/CadastroCozinhaIT.java
+++ src/test/java/br/com/wepdev/CadastroCozinhaIT.java
@@ -1,21 +1,22 @@
 package br.com.wepdev;
 
+import br.com.wepdev.domain.exception.CozinhaNaoEncontradaException;
+import br.com.wepdev.domain.exception.EntidadeEmUsoException;
 import br.com.wepdev.domain.model.Cozinha;
 import br.com.wepdev.domain.service.CozinhaService;
 
-import org.junit.Test;
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.Test; // Nova biblioteca de teste para JUnit
+//import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import javax.validation.ConstraintViolationException;
+
 
 
 /**
@@ -23,14 +24,14 @@ import javax.validation.ConstraintViolationException;
  */
 @RunWith(SpringRunner.class) // levanta o contexto do spring na hora dos testes
 @SpringBootTest // Fornece as funcionalidades do spring para os testes
-public class CadastroCozinhaIntegrationTests {
+public class CadastroCozinhaIT {
 
     @Autowired
     private CozinhaService cozinhaService;
 
 
     @Test
-    public void testarCadastroCozinhaComSucesso(){
+    public void deveAtribuirId_QuandocadastrarCozinhaComDadosCorretos(){
 
         // Cenario
         Cozinha novaCozinha = new Cozinha();
@@ -46,11 +47,26 @@ public class CadastroCozinhaIntegrationTests {
 
 
     @Test(expected = ConstraintViolationException.class) // Exception que tem que ser gerada
-    public void testarCadastroCozinhaSemNome(){
+    public void deveFalhar_QuandoCadastrarCozinhaSemNome(){
+
         Cozinha novaCozinha = new Cozinha();
+
         novaCozinha.setNome(null);
 
         novaCozinha = cozinhaService.salvar(novaCozinha);
     }
+
+
+    @Test(expected = EntidadeEmUsoException.class)
+    public void deveFalhar_QuandoExcluirCozinhaEmUso() {
+        cozinhaService.excluir(1L);
+    }
+
+
+    @Test(expected = CozinhaNaoEncontradaException.class)
+    public void deveFalhar_QuandoExcluirCozinhaInexistente() {
+        cozinhaService.excluir(100L);
+    }
+
 
 }
