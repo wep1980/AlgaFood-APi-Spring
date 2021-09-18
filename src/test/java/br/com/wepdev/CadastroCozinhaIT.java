55cbb8ea94b799ea29cc912a0e385d10eb44b724
wep1980 <wepbike@gmail.com>
2021-09-17 21:22:38 -0300

    Testes de API com REST Assured e validando o código de status HTTP

--- src/test/java/br/com/wepdev/CadastroCozinhaIT.java
+++ src/test/java/br/com/wepdev/CadastroCozinhaIT.java
@@ -7,8 +7,13 @@ import br.com.wepdev.domain.service.CozinhaService;
 
 //import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.assertj.core.api.Assertions.assertThat;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -22,51 +27,70 @@ import javax.validation.ConstraintViolationException;
 /**
  * Classe de testes de integração e testes de APi
  */
+//Fornece as funcionalidades do spring para os testes. webEnvironment -> levanta um servidor para uso dos testes, em uma porta aleatoria
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @RunWith(SpringRunner.class) // levanta o contexto do spring na hora dos testes
-@SpringBootTest // Fornece as funcionalidades do spring para os testes
 public class CadastroCozinhaIT {
 
-    @Autowired
-    private CozinhaService cozinhaService;
-
+    @LocalServerPort
+    private int port; // Variavel que recebe o numero da porta que foi levantado pelo servidor para teste
 
     @Test
-    public void deveAtribuirId_QuandocadastrarCozinhaComDadosCorretos(){
+    public void deveRetornarStatus200_QuandoConsultarCozinhas(){
 
-        // Cenario
-        Cozinha novaCozinha = new Cozinha();
-        novaCozinha.setNome("Chinesa");
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails(); // Habilita o logging
 
-        // Ação
-        novaCozinha = cozinhaService.salvar(novaCozinha);
-
-        // validação
-        assertThat(novaCozinha).isNotNull();
-        assertThat(novaCozinha.getId()).isNotNull();
+        RestAssured.given() // Dado que
+                    .basePath("/cozinhas") // eu tenho basePath
+                    .port(port) // na porta
+                    .accept(ContentType.JSON) // aceita o retorno em Json
+                .when() // Quando
+                    .get() // for feita uma requisição GET
+                    .then() // Então
+                    .statusCode(HttpStatus.OK.value()); // O status precisa ser 200
     }
 
-
-    @Test(expected = ConstraintViolationException.class) // Exception que tem que ser gerada
-    public void deveFalhar_QuandoCadastrarCozinhaSemNome(){
-
-        Cozinha novaCozinha = new Cozinha();
-
-        novaCozinha.setNome(null);
-
-        novaCozinha = cozinhaService.salvar(novaCozinha);
-    }
-
-
-    @Test(expected = EntidadeEmUsoException.class)
-    public void deveFalhar_QuandoExcluirCozinhaEmUso() {
-        cozinhaService.excluir(1L);
-    }
-
-
-    @Test(expected = CozinhaNaoEncontradaException.class)
-    public void deveFalhar_QuandoExcluirCozinhaInexistente() {
-        cozinhaService.excluir(100L);
-    }
+//    @Autowired
+//    private CozinhaService cozinhaService;
+//
+//
+//    @Test
+//    public void deveAtribuirId_QuandocadastrarCozinhaComDadosCorretos(){
+//
+//        // Cenario
+//        Cozinha novaCozinha = new Cozinha();
+//        novaCozinha.setNome("Chinesa");
+//
+//        // Ação
+//        novaCozinha = cozinhaService.salvar(novaCozinha);
+//
+//        // validação
+//        assertThat(novaCozinha).isNotNull();
+//        assertThat(novaCozinha.getId()).isNotNull();
+//    }
+//
+//
+//    @Test(expected = ConstraintViolationException.class) // Exception que tem que ser gerada
+//    public void deveFalhar_QuandoCadastrarCozinhaSemNome(){
+//
+//        Cozinha novaCozinha = new Cozinha();
+//
+//        novaCozinha.setNome(null);
+//
+//        novaCozinha = cozinhaService.salvar(novaCozinha);
+//    }
+//
+//
+//    @Test(expected = EntidadeEmUsoException.class)
+//    public void deveFalhar_QuandoExcluirCozinhaEmUso() {
+//        cozinhaService.excluir(1L);
+//    }
+//
+//
+//    @Test(expected = CozinhaNaoEncontradaException.class)
+//    public void deveFalhar_QuandoExcluirCozinhaInexistente() {
+//        cozinhaService.excluir(100L);
+//    }
 
 
 }
