eb65a462adaff91c56890593c477e118cf8ef001
wep1980 <wepbike@gmail.com>
2021-09-18 09:45:47 -0300

    Classe DatabaseCleaner para limpar de dados de teste e metodo para popular

--- src/test/java/br/com/wepdev/CadastroCozinhaIT.java
+++ src/test/java/br/com/wepdev/CadastroCozinhaIT.java
@@ -3,11 +3,13 @@ package br.com.wepdev;
 import br.com.wepdev.domain.exception.CozinhaNaoEncontradaException;
 import br.com.wepdev.domain.exception.EntidadeEmUsoException;
 import br.com.wepdev.domain.model.Cozinha;
+import br.com.wepdev.domain.repository.CozinhaRepository;
 import br.com.wepdev.domain.service.CozinhaService;
 
 //import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import br.com.wepdev.util.DatabaseCleaner;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.flywaydb.core.Flyway;
@@ -44,9 +46,15 @@ public class CadastroCozinhaIT {
     @LocalServerPort
     private int port; // Variavel que recebe o numero da porta que foi levantado pelo servidor para teste
 
-    // Instancia da classe flyWay, usada para ser feito o migrate(), que limpa e injeta os dados no banco de dados. Sera usado no metodo que é executado antes de cada teste
+//    // Instancia da classe flyWay, usada para ser feito o migrate(), que limpa e injeta os dados no banco de dados. Sera usado no metodo que é executado antes de cada teste
+//    @Autowired
+//    private Flyway flyway;
+
     @Autowired
-    private Flyway flyway;
+    private DatabaseCleaner databaseCleaner; // Intancia da classe responsavel por limpar os dados do banco de dados
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
 
 
     /**
@@ -59,11 +67,14 @@ public class CadastroCozinhaIT {
         RestAssured.port = port; // Porta gerada para os testes web. -- Para nao ficar duplicando em todos os testes
         RestAssured.basePath = "/cozinhas"; // Caminho. -- Para nao ficar duplicando em todos os testes
 
-        /**
-         * Executa o arquivo afeterMigrate.sql que esta dentro da pasta db.migration/testdata, esse arquivo limpa o banco e injeta eles novamente.
-         * afeterMigrate.sql -> E um arquivo de callBack, ja que ele zera o banco e reinsere os dados
-         */
-        flyway.migrate();
+//        /**
+//         * Executa o arquivo afeterMigrate.sql que esta dentro da pasta db.migration/testdata, esse arquivo limpa o banco e injeta eles novamente.
+//         * afeterMigrate.sql -> E um arquivo de callBack, ja que ele zera o banco e reinsere os dados
+//         */
+//        flyway.migrate();
+
+        databaseCleaner.clearTables(); // metodo que vai limpar os dados de todas as tabelas
+        prepararDados();
 
     }
 
@@ -85,7 +96,7 @@ public class CadastroCozinhaIT {
 
 
     @Test
-    public void deveConter4Cozinhas_QuandoConsultarCozinhas() {
+    public void deveConter2Cozinhas_QuandoConsultarCozinhas() {
 
         //RestAssured.enableLoggingOfRequestAndResponseIfValidationFails(); // Habilita o logging
 
@@ -96,8 +107,8 @@ public class CadastroCozinhaIT {
                 .when() // Quando
                 .get() // for feita uma requisição GET
                 .then() // Então
-                .body("", Matchers.hasSize(5)) // Verifica no corpo se tem 4 objs. Matchers -> biblioteca para inscrever expressoes com regras de correspondencia entre objetos
-                .body("nome" , Matchers.hasItems("Tailandesa", "Indiana")); // Verifica se no corpo os objetos possuem esses nomes
+                .body("", Matchers.hasSize(2)) // Verifica no corpo se tem 4 objs. Matchers -> biblioteca para inscrever expressoes com regras de correspondencia entre objetos
+                .body("nome" , Matchers.hasItems("Australiana", "Americana")); // Verifica se no corpo os objetos possuem esses nomes
 
     }
 
@@ -113,6 +124,22 @@ public class CadastroCozinhaIT {
                 .then() // então
                     .statusCode(HttpStatus.CREATED.value()); // O status deve ser criado
 
+
+    }
+
+
+    /**
+     * metodo responsavel por inserir uma massa de dados para rodar os testes
+     */
+    private void prepararDados(){
+
+        Cozinha cozinha1 = new Cozinha();
+        cozinha1.setNome("Australiana");
+        cozinhaRepository.save(cozinha1);
+
+        Cozinha cozinha2 = new Cozinha();
+        cozinha2.setNome("Americana");
+        cozinhaRepository.save(cozinha2);
 
     }
 
