f8375602dde9bc1d5eab7928a8d5e61200ae6057
wep1980 <wepbike@gmail.com>
2021-10-11 21:41:27 -0300

    Checando media type ao servir arquivos de fotos

--- src/main/java/br/com/wepdev/api/controller/RestauranteProdutoFotoController.java
+++ src/main/java/br/com/wepdev/api/controller/RestauranteProdutoFotoController.java
@@ -13,12 +13,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 
 @RestController
@@ -111,18 +113,41 @@ public class RestauranteProdutoFotoController {
 
 
     /**
-     * Classe que representa um InputStream de um recurso
+     * Classe que representa um InputStream de um recurso.
+     *
+     * Ao nao especificar o produces, qualquer tipo de MediaType que nao seja application/json vai cair neste endpoint.
+     *
+     * @RequestHeader(name = "accept") String acceptHeader -> Pega o que for especificado no accept do cabeçalho da requisição, que pode ser
+     * uma lista, exp : image/jpeg,image/png.
+     * (name = "accept") -> binding com String acceptHeader --- accept -> especificação do nome do header
+     *
+     * TIPOS DE MEDIATYPE passados no POSTMAN accept -> image/* - image/png - image/jpeg - image/gif - image/gif,image/jpeg,image/png
      */
-    @GetMapping(produces = MediaType.IMAGE_JPEG_VALUE)// Retorna um JPEG, caso o consumidor da APi informar a Accept application/jpeg
-    public ResponseEntity<InputStreamResource> servirFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId) {
+    @GetMapping//(produces = MediaType.IMAGE_JPEG_VALUE)// Retorna um JPEG, caso o consumidor da APi informar a Accept application/jpeg
+    public ResponseEntity<InputStreamResource> servirFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId,
+                                          @RequestHeader(name = "accept") String acceptHeader) throws HttpMediaTypeNotAcceptableException {
 
         try {
         FotoProduto fotoProduto = catalogoFotoProdutoService.buscarOuFalhar(restauranteId, produtoId);
 
+        /*
+        parseMediaType() -> converte uma String em um tipo MediaType.
+        MediaType == contentType -> são iguais
+         */
+        MediaType mediaTypeFoto = MediaType.parseMediaType(fotoProduto.getContentType());// MediaType da foto armazenada no disco e cadastrada no banco de dados
+
+        /*
+        mediasTypes aceitas pelo consumidor da Api, lista com os mediaTypes.
+        parseMediaTypes ->  converte uma lista de Strings em um tipo de lista de MediaTypes.
+         */
+        List<MediaType> mediatypesAceitas = MediaType.parseMediaTypes(acceptHeader);
+
+        verificarCompatibilidadeMediaType(mediaTypeFoto, mediatypesAceitas);
+
         InputStream inputStream = fotoStorageService.recuperar(fotoProduto.getNomeArquivo());
 
         return ResponseEntity.ok()
-                .contentType(MediaType.IMAGE_JPEG)
+                .contentType(mediaTypeFoto) // Retorna no cabeçalho o contentType exato que esta armazenado
                 .body(new InputStreamResource(inputStream));
 
         // Exception gerada caso a foto de um produto buscada nao exista no produto
@@ -131,6 +156,20 @@ public class RestauranteProdutoFotoController {
 
         }
 
+    }
+
+    private void verificarCompatibilidadeMediaType(MediaType mediaTypeFoto, List<MediaType> mediatypesAceitas) throws HttpMediaTypeNotAcceptableException {
+
+        /**
+         * Verifica se dentro da lista de mediasTypes aceitas tem pelo ao menos uma mediaType compativel com a mediaType da foto
+         */
+        boolean compativel = mediatypesAceitas.stream()
+                .anyMatch(mediaTypeAceita -> mediaTypeAceita.isCompatibleWith(mediaTypeFoto));
+
+        if(!compativel){
+            // HttpMediaTypeNotAcceptableException -> exception tratada pelo exceptionHandler
+            throw new HttpMediaTypeNotAcceptableException(mediatypesAceitas);
+        }
     }
 
 
