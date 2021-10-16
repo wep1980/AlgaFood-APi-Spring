7b2164b63c23b397a239c7ae63818d2626e2322a
wep1980 <wepbike@gmail.com>
2021-10-16 08:57:10 -0300

    Usando o serviço de email na confirmação de pedidos

--- src/main/java/br/com/wepdev/domain/service/EnvioEmailService.java
+++ src/main/java/br/com/wepdev/domain/service/EnvioEmailService.java
@@ -2,8 +2,9 @@ package br.com.wepdev.domain.service;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NonNull;
+import lombok.Singular;
 
-import javax.mail.search.SearchTerm;
 import java.util.Set;
 
 public interface EnvioEmailService {
@@ -16,8 +17,13 @@ public interface EnvioEmailService {
     @Builder
     class Mensagem{
 
+        @Singular // Anotação do lombok, singulariza o Set
         private Set<String> destinatarios;
+
+        @NonNull // Anotacao do lombok para validar que seja passado um assunto
         private String assunto;
+
+        @NonNull // Anotacao do lombok para validar que seja passado um corpo
         private String corpo;
     }
 }
--- src/main/java/br/com/wepdev/domain/service/FluxoPedidoService.java
+++ src/main/java/br/com/wepdev/domain/service/FluxoPedidoService.java
@@ -14,11 +14,30 @@ public class FluxoPedidoService {
 	private EmissaoPedidoService emissaoPedidoService;
 
 
+	@Autowired
+	private EnvioEmailService envioEmailService;
+
+
 
 	@Transactional
 	public void confirmar(String codigoPedido){
 		Pedido pedido = emissaoPedidoService.buscarOuFalhar(codigoPedido);
 		pedido.confirmar();
+
+		var mensagem = EnvioEmailService.Mensagem.builder().assunto(pedido.getRestaurante().getNome() + "-" + " - Pedido confirmado")
+
+						// Como no envio do corpo foi configurado como HTML, o <strong> que deixa em negrito, vai funcionar
+						.corpo("O pedido de código <strong>" + pedido.getCodigo() + "</strong> foi confirmado!")
+
+						/*
+						/ Aqui em desinario precisaria ser passado um Set() de destinatarios(), mas com a anotação do lombok colocado no Set, ele singularizou o metedo,
+						transformando de destinatarios() para destinatario(), ou seja para passar varios destinatarios é nessesario fazer :
+						destinatario("wepbike@gmail.com") e assim por diante.
+						 */
+			     		.destinatario(pedido.getCliente().getEmail())
+				        .build();
+
+        envioEmailService.enviar(mensagem);
 	}
 
 
