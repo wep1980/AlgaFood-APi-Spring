6f1f669e65484950e416d2ddd9a40d7206d6a051
wep1980 <wepbike@gmail.com>
2021-11-22 14:55:46 -0300

    Gerando a documentação da API em HTML com Swagger UI e SpringFox

--- src/main/java/br/com/wepdev/core/openapi/SpringFoxConfig.java
+++ src/main/java/br/com/wepdev/core/openapi/SpringFoxConfig.java
@@ -2,12 +2,15 @@ package br.com.wepdev.core.openapi;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
+// WebMvcConfigurer -> Interface usada para customizar o Spring MCV no projeto
 @Configuration
-public class SpringFoxConfig {
+public class SpringFoxConfig implements WebMvcConfigurer {
 
 
     /**
@@ -21,5 +24,21 @@ public class SpringFoxConfig {
                 .select() // Retorna um builder que ira selecionar os end points que devem ser expostos na definição do JSON
                 .apis(RequestHandlerSelectors.any()) // especifica os controladores e end points que serão escaneados, RequestHandlerSelectors.any() -> tudo que tiver na api
                 .build(); // retorna o Docket
+    }
+
+
+    /**
+     * Metodo que faz o mapeamento de caminhos para servir arquivos estaticos do springFox swagger ui
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //registry.addResourceHandler("swagger-ui.html") // Padrão de caminho recebido
+        registry.addResourceHandler("index.html") // Padrão de caminho recebido
+                .addResourceLocations("classpath:/META-INF/resources/"); // Arquivo que vai ser fornecido(servido)
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
     }
 }
