235bba7d835f7219ee471be7510e5a73096bdbd0
wep1980 <wepbike@gmail.com>
2021-11-24 08:30:57 -0300

    Selecionando os (caminhos) endpoints da API para gerar a documentação

--- src/main/java/br/com/wepdev/core/openapi/SpringFoxConfig.java
+++ src/main/java/br/com/wepdev/core/openapi/SpringFoxConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -16,13 +17,19 @@ public class SpringFoxConfig implements WebMvcConfigurer {
     /**
      * Metodo que disponibiliza uma instancia de Docket.
      * Docket -> Classe que representa a configuração da Api para utilizar a especificação openApi
-     * @return
+     *
+     * URL para acessar a pagina HTML do Swagger --- http://localhost:8080/swagger-ui/index.html
      */
     @Bean
     public Docket apiDocket() {
         return new Docket(DocumentationType.OAS_30)
                 .select() // Retorna um builder que ira selecionar os end points que devem ser expostos na definição do JSON
-                .apis(RequestHandlerSelectors.any()) // especifica os controladores e end points que serão escaneados, RequestHandlerSelectors.any() -> tudo que tiver na api
+
+                // especifica os controladores e end points que serão escaneados, RequestHandlerSelectors.any() -> todos os controladores da api
+                //.apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("br.com.wepdev.api")) // escaneia apenas os controladores desse pacote
+                .paths(PathSelectors.any()) // seleciona os caminhos dos end points que terao documentação, any() -> todos os caminhos
+                //.paths(PathSelectors.ant("/restaurantes/*"))
                 .build(); // retorna o Docket
     }
 
